e42bc410a9443fe24de3fbae590d045af25ffc3b
周浩 <hzzhouhao1@core.netease.com>
2019-05-22 13:40:38 +0800

    feat(clap): 解析命令

--- src/clap.js
+++ src/clap.js
@@ -1,6 +1,25 @@
 class Clap {
     constructor() {
+        this.args = '';
+        this.params = {};
+        this.command = null;
+    }
 
+    parse(args) {
+        this.args = args.trim();
+
+        this.command = this._parseCommand();
+    }
+
+    _parseCommand() {
+        const commandRe = /\b([^-]+)\s+-/g;
+        const result = commandRe.exec(this.args);
+
+        return result ? result[1] : null;
+    }
+
+    getParams() {
+        return this.params;
     }
 }
 
--- test/clap.test.js
+++ test/clap.test.js
@@ -1,9 +1,57 @@
 const {Clap} = require('../src/clap');
 
-describe('Clap test', function () {
-    test('constructor test', () => {
-        const clap = new Clap();
+test('constructor test', () => {
+    const clap = new Clap();
 
-        expect(clap).toBeInstanceOf(Clap);
+    expect(clap).toBeInstanceOf(Clap);
+});
+
+describe('command argument', () => {
+    let clap;
+    beforeEach(() => {
+        clap = new Clap();
     });
+
+    const cases = [
+        {commandLine: 'command --a --b', expectValue: 'command'},
+        {commandLine: '    command --a --b     ', expectValue: 'command'},
+        {commandLine: '--a', expectValue: null},
+        {commandLine: ' --a', expectValue: null}
+    ];
+
+    cases.forEach(({commandLine, expectValue}) => {
+        test(`'${commandLine}' => ${expectValue}`, () => {
+            clap.parse(commandLine);
+            expect(clap.command).toEqual(expectValue);
+        });
+    });
+});
+
+describe('boolean argument', () => {
+    let clap;
+    beforeEach(() => {
+        clap = new Clap();
+    });
+
+    test(`'--flag' => {flag: true}`, () => {
+        clap.parse('--flag');
+        expect(clap.getParams()).toEqual({
+            flag: true
+        });
+    });
+
+    test(`'--flag-boo' => {flagBoo: true}`, () => {
+        clap.parse('--flag-boo');
+        expect(clap.getParams()).toEqual({
+            flagBoo: true
+        });
+    });
+
+    test(`'   --flag     --flag-boo  ' => {flag: true, flagBoo: true}`, () => {
+
+    });
+});
+
+describe('string argument', () => {
+
 });
